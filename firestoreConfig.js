e0e73229c724ffba50c2990282d2ea3760763959
Eoin O'Kennedy <Eoin.OKennedy@tudublin.ie>
2025-11-03 15:53:57 +0000

    mostly done but not working behind firewall

--- firestoreConfig.js
+++ firestoreConfig.js
@@ -2,8 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Your Firebase configuration object goes here
-}
+    
+    "projectId": "wk8lab",
+    "appId": "1:1026375637269:web:abd06f15df83790630711c",
+    "storageBucket": "wk8lab.firebasestorage.app",
+    "apiKey": "AIzaSyBetmT5dYnjIagEEjOywbXTYxinI5JI0DY",
+    "authDomain": "wk8lab.firebaseapp.com",
+    "messagingSenderId": "1026375637269",
+    "projectNumber": "1026375637269",
+    "version": "2"
+  }
 
-const app = initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);    
 export const db = getFirestore(app);
